44b3ded391de537034f7ac95fe857110eabeab4d
yb17821 <37092406+yb17821@users.noreply.github.com>
2018-04-09 21:46:07 +0800

    增加了mongo写入

--- qunar_hotels/pipelines.py
+++ qunar_hotels/pipelines.py
@@ -5,24 +5,50 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import datetime
+import os
+import pymongo
+
 
 class QunarHotelsPipeline(object):
-    # def open_spider(self,spider):
-    #     self.f = open('hotel.json','w',encoding='utf-8')
-    # def close_spider(self, spider):
-    #     self.f.close()
+    def open_spider(self, spider):
+        today = datetime.datetime.now().strftime('%Y%m%d')
+        self.path = r'json/%s' % today
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        self.client = pymongo.MongoClient()
+        self.db = self.client.qunr
+        self.collection = self.db[today]
+
+    def close_spider(self, spider):
+        self.client.close()
+        self.db.close()
+
     def process_item(self, item, spider):
+
+        self.write_in_json(item)
+
+        return item
+
+    def write_in_mongo(self, hotel_info):
+
+
+        self.collection.insert(hotel_info)
+
+
+    def write_in_json(self,item):
         city_url = item['city_url'][0]
-        with open(r'json/%s.json' % city_url,'w',encoding='utf-8') as fp:
+        with open(os.path.join(self.path, city_url) + '.json', 'w', encoding='utf-8') as fp:
             fp.write('{')
             for hotel in item['result']:
                 hotel_info = {
+                    "city":city_url,
                     "dname": hotel[0],
                     "qname": hotel[1],
                     "group": hotel[2],
                     "groupType": hotel[3],
                     "hotDegree": hotel[4]
                 }
-                fp.write(json.dumps(hotel_info,ensure_ascii=False)+',\n')
+                fp.write(json.dumps(hotel_info, ensure_ascii=False) + ',\n')
+                self.write_in_mongo(hotel_info)
             fp.write('}')
-        return item
